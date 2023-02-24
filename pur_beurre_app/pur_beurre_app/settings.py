1505f50da8b07cb330fc30b18800a29298411ceb
R4mTex <poutots@gmail.com>
2023-02-24 10:42:00 +0100

    test problems

--- pur_beurre_app/pur_beurre_app/settings.py
+++ pur_beurre_app/pur_beurre_app/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
