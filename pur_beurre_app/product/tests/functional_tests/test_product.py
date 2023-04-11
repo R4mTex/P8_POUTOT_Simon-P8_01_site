175a8f3ae676a976e61f0c87631592190795e698
R4mTex <poutots@gmail.com>
2023-04-11 17:04:07 +0200

    add Travis files 1.2

--- pur_beurre_app/product/tests/functional_tests/test_product.py
+++ pur_beurre_app/product/tests/functional_tests/test_product.py
@@ -9,13 +9,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 class TestHome(StaticLiveServerTestCase):
     def setUp(self):
         options = webdriver.ChromeOptions()
-        options.binary_location = r"C:\Program Files\Google\Chrome\Application\112.0.5615.50\chrome.exe"
+        options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
         #chrome_driver_binary = r"C:\Users\spout\.wdm\drivers\chromedriver\win32\112.0.5615\chromedriver.exe"
         service = Service(r"C:\Users\spout\.wdm\drivers\chromedriver\win32\112.0.5615\chromedriver.exe")
         self.browser = webdriver.Chrome(service=service, options=options)
         #self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.browser.get(self.live_server_url + reverse("signup"))
-
 
     def tearDown(self):
         self.browser.close()
@@ -45,6 +44,7 @@ class TestHome(StaticLiveServerTestCase):
         login.click()
 
     def test_home_with_logged_user(self):
+        #self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.browser.get(self.live_server_url + reverse("signup"))
 
         username = self.browser.find_element("id", "id_username")
@@ -65,6 +65,7 @@ class TestHome(StaticLiveServerTestCase):
         )
 
     def test_home_with_not_logged_user(self):
+        #self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.browser.get(self.live_server_url + reverse("favorite-product"))
         self.assertEqual(
             self.browser.current_url,
